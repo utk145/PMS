d105d3ca4b213b84e2ab5891910e283b119374e5
utk145 <utkarshs14.2002@gmail.com>
2025-05-27 13:31:15 +0530

    request validation error

---
+++ patient-service/src/main/java/com/pms/patientservice/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.pms.patientservice.exceptions;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+//ControllerAdvice is a meta-annotation that indicates that the class is an advice class, and it lets us handle cross-cutting concerns such as exception handling, on both sides of controllers and services.
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
