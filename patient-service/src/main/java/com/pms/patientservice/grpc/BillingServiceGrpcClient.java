b1810f153f0c312aaaf7c06758cd733a8febe993
utk145 <utkarshs14.2002@gmail.com>
2025-12-07 15:02:20 +0530

    patient-service: building grpc client

---
+++ patient-service/src/main/java/com/pms/patientservice/grpc/BillingServiceGrpcClient.java
@@ -0,0 +1,43 @@
+package com.pms.patientservice.grpc;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BillingServiceGrpcClient {
+
+    private static final Logger logger = LoggerFactory.getLogger(BillingServiceGrpcClient.class);
+    private final BillingServiceGrpc.BillingServiceBlockingStub blockingStub;
+
+    public BillingServiceGrpcClient(
+            @Value("${billing.service.address:localhost}") String serverAddress,
+            @Value("${billing.service.grpc.port:9001}") int serverPort
+    ) {
+        logger.info("Connecting to Billing Service GRPC service at {}:{}", serverAddress, serverPort);
+
+        ManagedChannel managedChannel = ManagedChannelBuilder.forAddress(serverAddress, serverPort).usePlaintext().build();
+
+        blockingStub = BillingServiceGrpc.newBlockingStub(managedChannel);
+
+    }
+
+    public BillingResponse createBillingAccount(String patientId, String name, String email) {
+        BillingRequest billingRequest =
+                BillingRequest.newBuilder()
+                        .setPatientId(patientId)
+                        .setEmail(email)
+                        .setName(name)
+                        .build();
+
+        BillingResponse response = blockingStub.createBillingAccount(billingRequest);
+
+        logger.info("Received response from billing service via gRPC: {}", response);
+
+        return response;
+    }
+
+}
